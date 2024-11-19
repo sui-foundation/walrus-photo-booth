109447c9579113d37ac5b78c6abe27dba8620b77
Jason Takahashi <jmtakahashi@gmail.com>
2024-11-19 07:28:14 -0800

    add homepage with supbase get functionality.

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,8 @@
 'use client';
 
-import React, { useRef, useState, useEffect } from 'react';
+import { useRef, useState, useEffect } from 'react';
 import Image from 'next/image';
 import { Button } from '@/components/ui/button';
-import Link from 'next/link';
 import { createClient } from '@supabase/supabase-js';
 
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || '';
@@ -11,38 +10,66 @@ const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_KEY || '';
 
 const supabase = createClient(supabaseUrl, supabaseKey);
 
-const PhotoBooth: React.FC = () => {
+interface Event {
+  id: number;
+  created_at: string;
+  event_title: string;
+  admin_id: number;
+}
+
+const HomePage: React.FC = () => {
+  // const resolvedParams = use(params);
+  const [events, setEvents] = useState<Event[]>([]);
+  const [error, setError] = useState<Error | null>(null);
+
+  useEffect(() => {
+    const fetchEvents = async () => {
+      const { data: events, error } = await supabase.from('events').select('*');
+
+      if (error) {
+        setError(error);
+        console.error('Error fetching events:', error);
+      } else {
+        console.log(events);
+        setEvents((events as Event[]) || []);
+      }
+    };
+
+    fetchEvents();
+  }, []);
+
+  if (error) {
+    return <div>Error loading photos</div>;
+  }
+
   return (
-    <>
-      <div className='min-h-screen w-full flex flex-col items-center justify-center p-4 relative'>
-        <h1>Photo Booth Events</h1>
-        <div className='flex'>
-          <div className=''>1</div>
-          <div className=''>2</div>
-          <div className=''>3</div>
-          <div className=''>4</div>
-        </div>
-        <div className='flex'>
-          <div className=''>5</div>
-          <div className=''>6</div>
-          <div className=''>7</div>
-          <div className=''>8</div>
-        </div>
-        <div className='flex'>
-          <div className=''>9</div>
-          <div className=''>10</div>
-          <div className=''>11</div>
-          <div className=''>12</div>
-        </div>
-        <div className='flex'>
-          <div className=''>13</div>
-          <div className=''>4</div>
-          <div className=''>15</div>
-          <div className=''>16</div>
-        </div>
+    <main className='container mx-auto px-4 py-8'>
+      <div className='w-full flex items-center justify-between relative'>
+        <h1 className='text-3xl font-bold mb-8'>Photo Booth Events</h1>
+        <Button className='mb-4'>Login</Button>
       </div>
-    </>
+
+      <div className='w-80 m-auto flex flex-col items-center justify-center p-8 relative'>
+        {events.length > 0 &&
+          events.map((e) => (
+            <a
+              href={`photos/${e.id}`}
+              key={e.id}
+              className='w-full rounded-md text-white bg-black py-3 px-6 mb-4 text-center'
+            >
+              Event {e.id}
+            </a>
+          ))}
+
+        <a
+          href='#'
+          className='w-full rounded-md text-white bg-gray-700 py-3 px-6 mb-4 text-center'
+        >
+          Create Event
+        </a>
+      </div>
+    </main>
   );
 };
 
-export default PhotoBooth;
+export default HomePage;
