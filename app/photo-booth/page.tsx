6641cd1bf00cc3333f2f16922cf832d9c71e6b01
Jason Takahashi <jmtakahashi@gmail.com>
2024-11-24 03:59:00 -0800

    add Select ui component and add select event functionality to photo booth page.

--- app/photo-booth/page.tsx
+++ app/photo-booth/page.tsx
@@ -2,23 +2,93 @@
 
 import { useState, useEffect } from 'react';
 import { useCustomWallet } from '@/contexts/CustomWallet';
-import { useRouter } from 'next/navigation';
-
+import { createClient } from '@supabase/supabase-js';
 import PhotoBooth from '@/components/PhotoBooth';
 import Loading from '@/components/Loading';
 import ProfilePopover from '@/components/ProfilePopover';
 
-const PhotoBoothPage: React.FC = () => {
-  const { isConnected } = useCustomWallet();
-  const router = useRouter();
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from '@/components/ui/select';
 
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || '';
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_KEY || '';
+
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+interface Event {
+  id: number;
+  created_at: string;
+  event_title: string;
+  admin_id: number;
+}
+
+const PhotoBoothPage: React.FC = () => {
+  const { isConnected, emailAddress } = useCustomWallet();
   const [isLoading, setIsLoading] = useState(true);
+  const [currentAdminId, setCurrentAdminId] = useState<number | null>(null);
+  const [currentEvent, setCurrentEvent] = useState(null);
+  const [events, setEvents] = useState<Event[]>([]);
+  const [error, setError] = useState<Error | null>(null);
 
   useEffect(() => {
-    if (isConnected) {
-    }
-    setIsLoading(false);
-  }, [isConnected, router]);
+    const fetchCurrentAdmin = async () => {
+      setIsLoading(true);
+
+      const { data: admins, error } = await supabase
+        .from('admins')
+        .select('id')
+        .eq('email', emailAddress);
+
+      if (error) {
+        setError(error);
+        console.error('Error fetching admin info:', error);
+      } else {
+        if (admins[0]) setCurrentAdminId(admins[0].id);
+      }
+
+      setIsLoading(false);
+    };
+
+    fetchCurrentAdmin();
+  }, [emailAddress]);
+
+  useEffect(() => {
+    const fetchEvents = async () => {
+      setIsLoading(true);
+
+      if (currentAdminId) {
+        const { data: events, error } = await supabase
+          .from('events')
+          .select('*')
+          .eq('admin_id', currentAdminId);
+
+        if (error) {
+          setError(error);
+          console.error('Error fetching events:', error);
+        } else {
+          setEvents((events as Event[]) || []);
+        }
+      }
+
+      setIsLoading(false);
+    };
+
+    fetchEvents();
+  }, [currentAdminId]);
+
+  const handleSelect = (e) => {
+    setCurrentEvent(e);
+  };
+
+  if (error) {
+    return <div>Error loading events</div>;
+  }
 
   if (isLoading) {
     return <Loading />;
@@ -40,7 +110,25 @@ const PhotoBoothPage: React.FC = () => {
         <ProfilePopover />
       </div>
       <div className='w-full flex items-center justify-center grow p-4'>
-        <PhotoBooth />
+        {currentEvent ? (
+          <PhotoBooth currentEvent={currentEvent} />
+        ) : (
+          <Select onValueChange={handleSelect}>
+            <SelectTrigger className='w-[200px]'>
+              <SelectValue placeholder='Select an Event' />
+            </SelectTrigger>
+            <SelectContent>
+              <SelectGroup>
+                {events &&
+                  events.map((el: Event, index) => (
+                    <SelectItem key={index} value={el.id.toString()}>
+                      {el.event_title}
+                    </SelectItem>
+                  ))}
+              </SelectGroup>
+            </SelectContent>
+          </Select>
+        )}
       </div>
     </main>
   );
