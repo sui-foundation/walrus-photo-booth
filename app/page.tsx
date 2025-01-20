00cbcca55ea47ccf75ba648fa1ae7e8622735cf9
Jarren San Jose <jarrensj@users.noreply.github.com>
2025-01-20 12:31:51 -0800

    change button text: Create your own event

--- app/page.tsx
+++ app/page.tsx
@@ -129,7 +129,7 @@ const HomePage: React.FC = () => {
                 href='/addEvent'
                 className='inline-flex items-center justify-center rounded-md bg-primary px-4 py-2 text-sm font-medium text-white hover:bg-primary/90'
               >
-                Create a new event
+                Create your own event
               </Link>
               <Link
                 href='/photo-booth'
