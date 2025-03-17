a0fe7102487e7a448526ca69e0497b68e9dc7a69
Jarren San Jose <jarrensj@users.noreply.github.com>
2025-03-16 19:36:58 -0700

    update epochs to current max

--- app/api/upload/route.tsx
+++ app/api/upload/route.tsx
@@ -9,7 +9,7 @@ export async function POST(request: Request) {
     const buffer = Buffer.from(arrayBuffer);
 
     // upload to walrus
-    const response = await fetch(`${PUBLISHER_URL}/v1/blobs?epochs=5`, {
+    const response = await fetch(`${PUBLISHER_URL}/v1/blobs?epochs=183`, {
       method: 'PUT',
       body: buffer,
       headers: {
